e63f7a4e97509ea5880d336b68e298ed0f023bda
Koulba <thierrykoulba@gmail.com>
2025-10-30 12:56:05 +0100

    'init'

---
+++ README.md
@@ -0,0 +1,5 @@
+# infradon2
+
+Ce repository sert au cours d'infradon2 de la HEIG-VD.
+
+Son détenteur est Dylan Eray.
---
+++ Session4_TheWelcome.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import PouchDB from 'pouchdb'
+
+declare interface Post {
+  post_name: string;
+  post_content: string;
+  attributes: {
+    creation_date: any;
+  };
+}
+
+// Référence à la base de données
+const storage = ref()
+// Données stockées
+const postsData = ref<Post[]>([])
+
+// Initialisation de la base de données
+const initDatabase = () => {
+  console.log('=> Connexion à la base de données');
+                          
+  const db = new PouchDB('http://admin:1756@localhost:5984/infradon2_db')
+  if (db) {
+    console.log("Connecté à la collection : " + db?.name)
+    storage.value = db
+  } else {
+    console.warn('Echec lors de la connexion à la base de données')
+  }
+}
+
+// Récupération des données
+const fetchData = () => {
+  // TODO Récupération des données
+  // https://pouchdb.com/api.html#batch_fetch
+  // Regarder l'exemple avec function allDocs
+  // Remplir le tableau postsData avec les données récupérées
+}
+
+onMounted(() => {
+  console.log('=> Composant initialisé');
+  initDatabase()
+  fetchData()
+});
+
+</script>
+
+<template>
+  <h1>Fetch Data</h1>
+  <article v-for="post in postsData" v-bind:key="(post as any).id">
+    <h2>{{ post.post_name }}</h2>
+    <p>{{ post.post_content }}</p>
+  </article>
+</template>
